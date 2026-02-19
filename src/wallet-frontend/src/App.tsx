552ff1f24d57fbcf923e5936d86032e8b5020f27
Marius <privacyfirst555@proton.me>
2026-02-19 16:08:11 +0200

    Bring back the react app from main-old

---
+++ src/wallet-frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+async function fetchRandom(): Promise<number> {
+  try {
+    const response = await fetch("/wallet/random/");
+    const contents = await response.json();
+    return contents.number;
+  } catch (error) {
+    console.log(error);
+    return 0;
+  }
+}
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <Button
+        onClick={async () => {
+          setNumber(await fetchRandom());
+        }}
+      >
+        random number is {number}
+      </Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/wallet-frontend/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/wallet": {
+        target: "https://wallet-server.wallet.test",
+        changeOrigin: true,
+      },
+    },
+    host: true,
+    port: 8000,
+    allowedHosts: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
+});
