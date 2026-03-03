ac150a047615e5a2ed0001b797d64cace7eee502
Marius <privacyfirst555@proton.me>
2026-03-03 21:24:46 +0200

    Add CORS allow="*" template to trusted-list

--- src/trusted-list/app/app.py
+++ src/trusted-list/app/app.py
@@ -1,17 +1,28 @@
-import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from functools import partial
+from quart import Quart, Blueprint, jsonify
+from quart_cors import cors
 
-SERVICE_NAME = os.getenv("SERVICE_NAME", "Unknown Service")
+FIRST_DOMAIN = 'trusted-list.wallet.test'
+SECOND_DOMAIN = 'public.trusted-list.wallet.test'
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        response = f"{SERVICE_NAME}\n"
-        self.wfile.write(response.encode("utf-8"))
+app = Quart(__name__, host_matching=True, static_host=FIRST_DOMAIN)
 
-if __name__ == "__main__":
-    server = HTTPServer(("0.0.0.0", 8000), Handler)
-    print(f"Server running on port 8000... (SERVICE_NAME={SERVICE_NAME})")
-    server.serve_forever()
+main = Blueprint('main', __name__)
+public = Blueprint('public', __name__)
+main_route = partial(main.route, host=FIRST_DOMAIN)
+public_route = partial(public.route, host=SECOND_DOMAIN)
+cors(public, allow_origin="*")
+
+@main_route('/')
+def main_index():
+    return jsonify({"message": f"Hello from {FIRST_DOMAIN}! Main site (protected)"})
+
+@public_route('/')
+def public_index():
+    return  jsonify({"message": f"Hello from {SECOND_DOMAIN}! API (public)"})
+
+app.register_blueprint(main)
+app.register_blueprint(public)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
