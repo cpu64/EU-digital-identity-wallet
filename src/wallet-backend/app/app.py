5812597f965d8f1a5f018b4faa30f98d3e4e07b4
pppss102 <73379683+pppss102@users.noreply.github.com>
2026-03-06 12:37:33 +0200

    add user login/register endpoints

--- src/wallet-backend/app/app.py
+++ src/wallet-backend/app/app.py
@@ -1,12 +1,64 @@
-import random
-from quart import jsonify, Quart
+import secrets
+import argon2
+from app import db
+from quart import jsonify, Quart, current_app, request
+from quart_auth import QuartAuth
 
 app = Quart(__name__)
+app.config["QUART_AUTH_MODE"] = "bearer"
+app.secret_key = secrets.token_urlsafe(16)
+auth = QuartAuth(app)
 
+@app.before_serving
+async def startup():
+    app.db_engine = await db.init_db()
 
-@app.route("/wallet/random/")
-async def get_random():
-    return jsonify({"number": random.randint(0, 10)})
+@app.after_serving
+async def shutdown():
+    await app.db_engine.dispose()
+
+def is_user_request_valid(req):
+    return req is not None and "username" in req and "password" in req
+
+@app.route("/api/register", methods=["POST"])
+async def register():
+    req = await request.get_json()
+
+    if not is_user_request_valid(req):
+        return jsonify({"success": False, "status": "Invalid input JSON"}), 400
+
+    hasher = argon2.PasswordHasher()
+    password_hash = hasher.hash(req["password"])
+
+    try:
+        await db.create_user(current_app.db_engine, req["username"], password_hash)
+    except Exception:
+        return jsonify({"success": False, "status": "Failed to create user"}), 400
+
+    return jsonify({"success": True, "Status": "User created successfully"})
+
+@app.route("/api/login", methods=["POST"])
+async def login():
+    req = await request.get_json()
+
+    if not is_user_request_valid(req):
+        return jsonify({"error": "Invalid input JSON"}), 400
+
+    user = await db.get_user(current_app.db_engine, req["username"])
+
+    if user is None:
+        return jsonify({"error": "Invalid username"}), 400
+
+    hasher = argon2.PasswordHasher()
+
+    try:
+        print(user["password_hash"])
+        hasher.verify(user["password_hash"], req["password"])
+    except Exception:
+        return jsonify({"error": "Invalid password"}), 400
+
+    token = auth.dump_token(user["username"])
+    return jsonify({"token": token})
 
 if __name__ == "__main__":
     app.run()
